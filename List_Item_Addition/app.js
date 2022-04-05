2265a992922614b7e0c2d199ee189a88f9788253
Devesh <devesh224.cavisson@gmail.com>
2022-04-05 15:09:15 +0530

    update and delete first child

--- List_Item_Addition/app.js
+++ List_Item_Addition/app.js
@@ -3,6 +3,9 @@ var formSection = document.getElementById("form-section");
 var btn = document.getElementById("btn");
 var inputField = document.getElementById("input-field");
 var currentInput;
+var updateBtn= document.getElementById("update-btn");
+var delBtn = document.getElementById("del-btn");
+
 
 inputField.addEventListener("input", function(e){
     currentInput= e.target.value;
@@ -14,16 +17,23 @@ if(e.keyCode===13){
 }});
 
 
-function addItemList(){
-    if(currentInput!== null && currentInput!== undefined && currentInput!== "")
-    {
+function createNewNode(){
     var newElement = document.createElement("li");
     var text = currentInput;
     var addThis = document.createTextNode(text);
     newElement.appendChild(addThis);
-    listItems.appendChild(newElement);
     inputField.value = '';
     currentInput = '';
+    return newElement;
+}
+
+
+function addItemList(){
+    if(currentInput!== null && currentInput!== undefined && currentInput!== "")
+    {
+    var newElement = createNewNode();
+    listItems.appendChild(newElement);
+    
 }
 else{
     alert("You need to enter some value in input ");
@@ -31,3 +41,27 @@ else{
 }
 
 btn.addEventListener("click", addItemList)
+
+
+
+updateBtn.addEventListener("click",function(){
+    if(currentInput!== null && currentInput!== undefined && currentInput!== ""){
+    var firstElement = listItems.firstElementChild;
+    var newListElement = createNewNode();
+    listItems.replaceChild(newListElement,firstElement);
+}
+else{
+    alert("You need to enter some value in input ");
+}
+})
+
+
+delBtn.addEventListener("click", function(){
+    var firstElement = listItems.firstElementChild;
+    if(listItems.childElementCount===1){
+        alert("Can't Remove Last Item")
+    }
+    else{
+        listItems.removeChild(firstElement);
+    }
+})
